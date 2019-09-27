4baa24f5f0402a7bce9c14f72fac1ae75241042c
Henry Shen <hk.shen94@gmail.com>
2019-09-27 20:25:48 +0200

    done

--- HighestScoringWord.js
+++ HighestScoringWord.js
@@ -1,4 +1,18 @@
 
 exports.highestScoringWord = (word) => {
-
+  arr = word.split(' ');
+  let hs = 0;
+  let hword;
+  for (let i =0; i<arr.length; i++) {
+    let sum = 0;
+    for (let j =0; j<arr[i].length; j++) {
+      sum = sum + arr[i].charCodeAt(j)-96;
+      //console.log(sum);
+    }
+    if (sum > hs) {
+      hs = sum;
+      hword = arr[i];
+    }
   }
+  return hword;
+}
